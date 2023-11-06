597ecfdbcc2f31b267a6d7ebaf904fce8fce6694
samade747 <samad.e747@gmail.com>
2023-11-06 19:39:40 +0500

    15266

--- app.js
+++ app.js
@@ -2,26 +2,26 @@ const reviews = [
 { id: '01',
   section: 'history',
   detail: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis dolorum voluptates eum. Illo odio nisi esse laudantium excepturi nesciunt dolorum numquam placeat repudiandae rem non, beatae harum sint, impedit dolor.atur',
-  image:  src="./images/00001.PNG"
-},
+  image: src="./images/00001.PNG"},
 
 { id: '02',
   section: 'Vision',
   detail: 'ipsum dolor sit amet consectetur, adipisicing elit. Omnis dolorum voluptates eum. Illo odio nisi esse laudantium excepturi nesciunt dolorum numquam placeat repudiandae rem non, beatae harum sint, impedit dolor.atur',
-  image:  src="./images/1697265904430.jpeg"
+  image: src="./images/1697265904430.jpeg"
 },
 
 { id: '03',
   section: 'Goals',
   detail: 'dolor sit amet consectetur, adipisicing elit. Omnis dolorum voluptates eum. Illo odio nisi esse laudantium excepturi nesciunt dolorum numquam placeat repudiandae rem non, beatae harum sint, impedit dolor.atur',
-  image:  src="./images/Capture321.PNG"
+  image: src="./images/Capture321.PNG"
 }
 
 ]
 
-let currIndex = 0;
+let currIndex = 2;
 
 let imageArea = document.getElementById('ImageArea');
 let boxtext = document.getElementById('boxtext');
 
-
+imageArea.src = reviews[currIndex].image;
+boxtext.textContent = reviews[currIndex].detail;

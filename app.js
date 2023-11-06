4ded51307b27284f8aa373415f13e8cd9d34702b
samade747 <samad.e747@gmail.com>
2023-11-06 19:35:04 +0500

    161

---
+++ app.js
@@ -0,0 +1,27 @@
+const reviews = [
+{ id: '01',
+  section: 'history',
+  detail: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis dolorum voluptates eum. Illo odio nisi esse laudantium excepturi nesciunt dolorum numquam placeat repudiandae rem non, beatae harum sint, impedit dolor.atur',
+  image:  src="./images/00001.PNG"
+},
+
+{ id: '02',
+  section: 'Vision',
+  detail: 'ipsum dolor sit amet consectetur, adipisicing elit. Omnis dolorum voluptates eum. Illo odio nisi esse laudantium excepturi nesciunt dolorum numquam placeat repudiandae rem non, beatae harum sint, impedit dolor.atur',
+  image:  src="./images/1697265904430.jpeg"
+},
+
+{ id: '03',
+  section: 'Goals',
+  detail: 'dolor sit amet consectetur, adipisicing elit. Omnis dolorum voluptates eum. Illo odio nisi esse laudantium excepturi nesciunt dolorum numquam placeat repudiandae rem non, beatae harum sint, impedit dolor.atur',
+  image:  src="./images/Capture321.PNG"
+}
+
+]
+
+let currIndex = 0;
+
+let imageArea = document.getElementById('ImageArea');
+let boxtext = document.getElementById('boxtext');
+
+

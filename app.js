c00e851f4d607f9fbbe31056feeecac936224f9a
samade747 <samad.e747@gmail.com>
2023-11-06 20:35:23 +0500

    5664115

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const reviews = [
 { id: '01',
   section: 'history',
-  detail: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis dolorum voluptates eum. Illo odio nisi esse laudantium excepturi nesciunt dolorum numquam placeat repudiandae rem non, beatae harum sint, impedit dolor.atur',
+  detail: "History is the record of humanity's past, documenting events, societies, and individuals that shaped our world. It provides valuable insights into our roots, cultures, and progress. Studying history helps us understand the present and make informed decisions for the future, ensuring that we learn from our collective experiences.",
   image:  src= "./images/history.jpeg"},
 
 { id: '02',
   section: 'Vision',
-  detail: 'ipsum dolor sit amet consectetur, adipisicing elit. Omnis dolorum voluptates eum. Illo odio nisi esse laudantium excepturi nesciunt dolorum numquam placeat repudiandae rem non, beatae harum sint, impedit dolor.atur',
+  detail: "Vision is the ability to perceive and interpret our surroundings, crucial for navigating the world. It extends beyond physical sight, encompassing aspirations and goals. A clear vision guides us, providing purpose and direction. It empowers individuals and organizations to strive for a better future, driving innovation and growth.",
   image: src="./images/1697265904430.jpeg"
 },
 
 { id: '03',
   section: 'Goals',
-  detail: 'dolor sit amet consectetur, adipisicing elit. Omnis dolorum voluptates eum. Illo odio nisi esse laudantium excepturi nesciunt dolorum numquam placeat repudiandae rem non, beatae harum sint, impedit dolor.atur',
+  detail: "Goals are the targets we set, reflecting our aspirations and desires. They provide direction and purpose, motivating us to take action and achieve our dreams. Whether personal or professional, well-defined goals serve as a roadmap, helping us measure progress and celebrate achievements along the way.",
   image: src="./images/Capture321.PNG"
 }
 
